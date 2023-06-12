ca646db20f3d23ef1caaf95b7ace55be0220302f
Dimebagou <bersonvictor@gmail.com>
2023-06-12 19:07:11 +0200

    endpoints prêts pour premiers tests en front

--- Querries/Associations/associations.js
+++ Querries/Associations/associations.js
@@ -120,12 +120,12 @@ export async function deleteGroup(group_id) {
     // supprime les paires associées avant dans users_groups et members_groups
     const [result] = await pool.query(
         'DELETE FROM users_groups WHERE group_id = ?',
-        [group_id]  
+        [group_id]
     )
     const [result2] = await pool.query(
         'DELETE FROM members_groups WHERE group_id = ?',
         [group_id]
-    )   
+    )
     // supprime le groupe
     const [result3] = await pool.query(
         'DELETE FROM groups WHERE id = ?',
@@ -174,6 +174,8 @@ export async function getMemberDetailsById(id) {
     return rows[0];
 }
 
+
+
 // retourne une adresse en fonction de son id
 export async function getAddressById(id) {
     const [rows] = await pool.query('SELECT * FROM addresses WHERE id = ?', [id]);
@@ -212,11 +214,55 @@ export async function createMemberDetails(address_id, mail, birthday, contraindi
 }
 
 // ajoute un membre
-export async function createMember(firstname, lastname, file_status, payment_status, member_details_id, photo, association_id, certificate, subscription, paid) {
+export async function createMember(street, postal_code, city, mail, birthday, contraindication, phone_number, emergency_number, birthplace, living_with, image_rights_signature, firstname, lastname, file_status, payment_status, photo, association_id, certificate, subscription, paid) {
+    // Créer une nouvelle adresse
+    const address = await createAddress(street, postal_code, city);
+
+    // Créer les détails du membre en utilisant l'ID de l'adresse créée
+    const memberDetails = await createMemberDetails(address, mail, birthday, contraindication, phone_number, emergency_number, birthplace, living_with, image_rights_signature);
+
+    // Créer le membre en utilisant les détails du membre créés précédemment
     const [result] = await pool.query(`
-        INSERT INTO members (firstname, lastname, file_status, payment_status, member_details_id, photo, association_id, certificate, subscription, paid)
-        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+    INSERT INTO members (firstname, lastname, file_status, payment_status, member_details_id, photo, association_id, certificate, subscription, paid)
+    VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+`,
+        [firstname, lastname, file_status, payment_status, memberDetails, photo, association_id, certificate, subscription, paid]);
+    return result;
+}
+
+// Modifie une adresse existante
+export async function updateAddress(addressId, street, postalCode, city) {
+    await pool.query(`
+        UPDATE addresses
+        SET street = ?, postal_code = ?, city = ?
+        WHERE id = ?
     `,
-        [firstname, lastname, file_status, payment_status, member_details_id, photo, association_id, certificate, subscription, paid]);
-    return getMemberById(result.insertId, association_id);
+        [street, postalCode, city, addressId]);
+}
+
+// Modifie les détails d'un membre existant
+export async function updateMemberDetails(detailsId, addressId, mail, birthday, contraindication, phoneNumber, emergencyNumber, birthplace, livingWith, imageRightsSignature) {
+    await pool.query(`
+        UPDATE member_details
+        SET address_id = ?, mail = ?, birthday = ?, contraindication = ?, phone_number = ?, emergency_number = ?, birthplace = ?, living_with = ?, image_rights_signature = ?
+        WHERE id = ?
+    `,
+        [addressId, mail, birthday, contraindication, phoneNumber, emergencyNumber, birthplace, livingWith, imageRightsSignature, detailsId]);
+}
+
+// Modifie un membre existant
+export async function updateMember(member_id, address_id, member_details_id, street, postal_code, city, mail, birthday, contraindication, phone_number, emergency_number, birthplace, living_with, image_rights_signature, firstname, lastname, file_status, payment_status, photo, association_id, certificate, subscription, paid) {
+    // Modifier l'adresse existante
+    await updateAddress(address_id, street, postal_code, city);
+
+    // Modifier les détails du membre existant
+    await updateMemberDetails(member_details_id, address_id, mail, birthday, contraindication, phone_number, emergency_number, birthplace, living_with, image_rights_signature);
+
+    // Modifier le membre
+    await pool.query(`
+        UPDATE members
+        SET firstname = ?, lastname = ?, file_status = ?, payment_status = ?, member_details_id = ?, photo = ?, association_id = ?, certificate = ?, subscription = ?, paid = ?
+        WHERE id = ?
+    `,
+        [firstname, lastname, file_status, payment_status, member_details_id, photo, association_id, certificate, subscription, paid, member_id]);
 }
--- Routes/Associations/members.js
+++ Routes/Associations/members.js
@@ -5,9 +5,9 @@ import {
     getMemberById,
     getMemberDetailsById,
     getAddressById,
-    createAddress,
-    createMemberDetails,
-    createMember
+    createMember,
+    updateMember,
+    getAssociation,
 } from '../../Querries/Associations/associations.js'
 
 const router = express.Router()
@@ -27,35 +27,22 @@ router.get("/", async (req, res) => {
 router.get("/:member_id", async (req, res) => {
     try {
         const member = await getMemberById(req.params.member_id, req.associationId);
+        const member_detail = await getMemberDetailsById(member.member_details_id);
+        const address = await getAddressById(member_detail.address_id);
+
+        member.member_detail = member_detail;
+        member.address = address;
+
         res.send(member);
     } catch (error) {
         console.error(error);
-        res.status(500).send("Une erreur est survenue lors de la récupération du membre.");
+        res.status(500).send("Une erreur est survenue lors de la récupération des informations du membre.");
     }
 });
 
-router.get("/:member_id/details", async (req, res) => {
-    try {
-        const member = await getMemberById(req.params.member_id, req.associationId);
-        const member_detail = await getMemberDetailsById(member.member_details_id);
-        res.send(member_detail);
-    } catch (error) {
-        console.error(error);
-        res.status(500).send("Une erreur est survenue lors de la récupération des détails du membre.");
-    }
-});
 
-router.get("/:member_id/address", async (req, res) => {
-    try {
-        const member = await getMemberById(req.params.member_id, req.associationId);
-        const member_detail = await getMemberDetailsById(member.member_details_id);
-        const address = await getAddressById(member_detail.address_id);
-        res.send(address);
-    } catch (error) {
-        console.error(error);
-        res.status(500).send("Une erreur est survenue lors de la récupération de l'adresse du membre.");
-    }
-});
+
+
 
 
 // POST // 
@@ -64,21 +51,46 @@ router.post("/", async (req, res) => {
     try {
         const { street, postal_code, city, mail, birthday, contraindication, phone_number, emergency_number, birthplace, living_with, image_rights_signature, firstname, lastname, file_status, payment_status, photo, certificate, subscription, paid } = req.body;
 
-        // Créer une nouvelle adresse
-        const address = await createAddress(street, postal_code, city);
-        
-        // Créer les détails du membre en utilisant l'ID de l'adresse créée
-        const memberDetails = await createMemberDetails(address, mail, birthday, contraindication, phone_number, emergency_number, birthplace, living_with, image_rights_signature);
+        // Créer le membre avec toutes les informations
+        const member = await createMember(street, postal_code, city, mail, birthday, contraindication, phone_number, emergency_number, birthplace, living_with, image_rights_signature, firstname, lastname, file_status, payment_status, photo, req.associationId, certificate, subscription, paid);
 
-        // Créer le membre en utilisant les détails du membre créés précédemment
-        const member = await createMember(firstname, lastname, file_status, payment_status, memberDetails, photo, req.associationId, certificate, subscription, paid);
 
-        res.status(201).send(member);
+        const new_member = firstname + " " + lastname;
+        const association = await getAssociation(req.associationId);
+        const association_name = association.name;
+
+
+        res.status(201).send(`Le membre ${new_member} a bien été ajouté à l'association ${association_name}.`);
     } catch (error) {
         console.error(error);
         res.status(500).send("Une erreur est survenue lors de la création du membre.");
     }
 });
+
+// PUT //
+
+router.put("/:member_id", async (req, res) => {
+    try {
+        const { street, postal_code, city, mail, birthday, contraindication, phone_number, emergency_number, birthplace, living_with, image_rights_signature, firstname, lastname, file_status, payment_status, photo, certificate, subscription, paid } = req.body;
+        const { member_id } = req.params;
+
+        const address = await getAddressById(req.associationId);
+        const address_id = address.id;
+        const member_details =  await getMemberDetailsById(member_id, req.associationId); 
+        const member_details_id = member_details.id;
+
+
+
+        // Mettre à jour les informations du membre
+        await updateMember(member_id, address_id, member_details_id, street, postal_code, city, mail, birthday, contraindication, phone_number, emergency_number, birthplace, living_with, image_rights_signature, firstname, lastname, file_status, payment_status, photo, req.associationId, certificate, subscription, paid);
+
+        res.status(200).send(`Les informations du membre avec l'ID ${member_id} ont été mises à jour.`);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send("Une erreur est survenue lors de la mise à jour des informations du membre.");
+    }
+});
+
 
 
 
