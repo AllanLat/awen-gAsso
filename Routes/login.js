bb8b808c18861642619da3b81afbb8c82297e31a
Dimebagou <bersonvictor@gmail.com>
2023-06-20 17:05:22 +0200

    bug comparaison password fixée

--- Routes/login.js
+++ Routes/login.js
@@ -9,9 +9,9 @@ const login = async (req, res, next) => {
         if (!user) {
             return res.status(401).json({ message: 'Connexion non autorisée.' })
         }
-
-        const isValid = await (req.body.password, user.password)
-        /* const isValid = bcrypt.compareSync(req.body.password, user.password) */
+        
+        const isValid = await (req.body.password === user.password)
+        /* const isValid = bcrypt.compareSync(req.body.password === user.password) */
         if (!isValid) {
             return res.status(401).json({ message: 'Connexion non autorisée.' })
         }
