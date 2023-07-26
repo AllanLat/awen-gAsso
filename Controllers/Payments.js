e070722950c0e33f7efb28046605936c0d23eb1c
Dimebagou <bersonvictor@gmail.com>
2023-07-26 16:48:00 +0200

    suppresion fichier compta + fix addPayment

--- Controllers/Payments.js
+++ Controllers/Payments.js
@@ -6,33 +6,36 @@ const viewAllPayments = async (req, res) => {
         return res.status(403).json("Vous n'avez pas les droits d'accès.");
     }
     try {
-       const pay = await viewAll(req.auth.associationId)
-       res.json(pay)
+        const pay = await viewAll(req.auth.associationId)
+        res.json(pay)
     } catch (error) {
         res.status(500).json("Une erreur est survenue lors de la récupération des transactions.");
     }
-} 
+}
 
 const addNewPayment = async (req, res) => {
 
-    
+
     if (req.auth.userLvl !== 1) {
         return res.status(403).json("Vous n'avez pas les droits d'accès.");
     }
     try {
-        await addPayment(req.auth.associationId,
-             req.body.credit,
-             req.body.debit,
-             req.body.payment_method,
-             req.body.payment_date, 
-             req.body.description,
-             req.body.balance)
+        const newPayment = await addPayment(req.auth.associationId,
+            req.body.credit,
+            req.body.debit,
+            req.body.payment_method,
+            req.body.payment_date,
+            req.body.description,
+            req.body.balance)
 
         
-        res.status(200).json({msg : "transaction ok."})
-        
+        res.status(201).json(newPayment)
+
+
+
     } catch (err) {
-        res.status(500).json({error: err});
+        console.log(err)
+        res.status(500).json({ error: err });
     }
 }
 
@@ -41,11 +44,11 @@ const viewAssociationBalance = async (req, res) => {
     if (req.auth.userLvl !== 1) {
         return res.status(403).json("Vous n'avez pas les droits d'accès.");
     }
-    
-    try{
+
+    try {
         const appel = await getBalance(req.auth.associationId)
         res.json(appel)
-    }catch(err){
+    } catch (err) {
         console.log(err)
     }
 }
--- Controllers/compta.js
+++
@@ -1,1 +0,0 @@
-/* Controller de la route compta */
