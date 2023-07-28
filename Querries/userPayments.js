8eb2ee850f5d6b5dbf7098b9c5ce099a08559f81
Clement Paya <clementpaya21@gmail.com>
2023-07-28 14:02:15 +0200

    Query avec jointure pour user payments

--- Querries/userPayments.js
+++ Querries/userPayments.js
@@ -2,7 +2,7 @@ import pool from "../Utils/pool.js"
 
 export async function viewAllMemberPayments(member_id) {
 
-    const [rows] = await pool.query('SELECT * FROM members_payments WHERE member_id = ? ORDER BY id DESC', 
+    const [rows] = await pool.query('SELECT * FROM members_payments JOIN payments ON members_payments.payment_id = payments.id WHERE member_id = ? ORDER BY payment_date DESC', 
     [member_id])
 
     return rows
