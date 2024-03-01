7fee46ff6f1a8b651d51cd005eb009c896372922
Bargid <120350488+Bargid@users.noreply.github.com>
2024-03-01 16:14:37 -0500

    Modifications pour bybass le login

--- Middleware/auth.mjs
+++ Middleware/auth.mjs
@@ -1,26 +1,23 @@
-import jwt from 'jsonwebtoken';
-import dotenv from 'dotenv';
-
-dotenv.config();
-
 const auth = (req, res, next) => {
-    try {
-
-        const token = req.headers.authorization.split(' ')[1];
-        const decodedToken = jwt.verify(token, process.env.CRYPT);
-
-
-        const userId = decodedToken.userId;
-        const associationId = decodedToken.associationId;
-        const userLvl = decodedToken.userLvl;
-
-        req.auth = { userId, associationId, userLvl };
+    // Allow all requests without authentication check during development
+    if (process.env.NODE_ENV === 'development') {
         next();
+    } else {
+        // For other environments, perform authentication as usual
+        try {
+            const token = req.headers.authorization.split(' ')[1];
+            const decodedToken = jwt.verify(token, process.env.CRYPT);
 
-    } catch (error) {
-        res.status(401).json({ error:"Unauthorized" }); 
-     
-    } 
+            const userId = decodedToken.userId;
+            const associationId = decodedToken.associationId;
+            const userLvl = decodedToken.userLvl;
+
+            req.auth = { userId, associationId, userLvl };
+            next();
+        } catch (error) {
+            res.status(401).json({ error: "Unauthorized" });
+        }
+    }
 }
 
-export default auth
+export default auth;
